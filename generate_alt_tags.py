4118d79d462925bdc491990ddc31073dfc8d1f89
victor <victor.lvl47@gmail.com>
2023-05-03 14:36:53 -0600

    First Commit

---
+++ generate_alt_tags.py
@@ -0,0 +1,17 @@
+from storage_service import StorageService
+from recognition_service import RecognitionService
+
+storage_service = StorageService()
+recognition_service = RecognitionService()
+
+bucket_name = '>>your.s3.bucket.name.here<<'
+
+for file in storage_service.get_all_files(bucket_name):
+    if file.key.endswith('.jpg'):
+        print('Image: ' + file.key)
+        labels = recognition_service.detect_objects(file.bucket_name, file.key)
+
+        print("Alt tags for this image: ")
+        for label in labels:
+            print(label['Name'])
+        print()  # Add newline
